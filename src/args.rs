efd389116e4427ccf99089b61a7280e280d9b74f
notohh <github@notohh.dev>
2024-06-01 14:27:38 -0400

    fix typo

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct Args {
     #[arg(short)]
-    pub(crate) length: usize,
+    pub length: usize,
     #[arg(short)]
     pub write_to_file: bool,
     #[arg(short, default_value_t = String::from("generated_pwd.txt"))]
